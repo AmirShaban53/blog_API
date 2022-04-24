de767c1d4da80493cb16d84a7f363daf1e056665
unknown <amirshaban53@gmail.com>
2022-04-24 07:32:30 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
-
 import { cloudinaryConfig } from './config/cloudinaryConfig.js';
 import posts from './routes/posts.js';
 import auth from './routes/auth.js';
-
 import logger from './middleware/logger.js';
-
-
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -18,19 +13,15 @@ const swaggerDocs = YAML.load('./src/config/swagger.yml');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-
 app.use(express.json());
 app.use(cors());
 app.use('*', cloudinaryConfig);
-app.use('/uploads', express.static('uploads'))
 app.get('/', (req, res)=> {res.json('welcome to the blog API.')})
-
-
+app.use('/uploads', express.static('uploads'))
 
 app.use('/posts', posts);
 app.use('/auth', auth);
-
-
+app.use((req, res)=>{res.status(404).json({message: "route not located"});});
 
 app.listen(PORT, ()=> {logger.info(`blog API now live at: http://localhost:${PORT}`)});
 
